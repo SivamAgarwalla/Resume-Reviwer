eeb137a75c5ab5dad44131f373571d47da4e052c
Sahana Ilenchezhian <sahana.chelian@gmail.com>
2021-01-09 18:44:37 -0800

    added firebase

---
+++ resumer-frontend/resume-reviewer/src/components/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyDeVSYKbjkVjulyuNuI87kbjDE-dtO2WOo",
+    authDomain: "resume-reviewer-4642a.firebaseapp.com",
+    projectId: "resume-reviewer-4642a",
+    storageBucket: "resume-reviewer-4642a.appspot.com",
+    messagingSenderId: "677080546556",
+    appId: "1:677080546556:web:777e1b986de477b9e92b79",
+    measurementId: "G-H11224KPZF"
+};
+firebase.initializeApp(config);
+
+export default firebase;
